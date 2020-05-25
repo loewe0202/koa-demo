a4041eb7d84af76a1e4d47de0ec359035350ba2a
loewe0202 <loewe0202@loewe0202deMacBook-Pro.local>
2020-05-25 23:28:58 +0800

    koa-helmet koa-static

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 const Koa = require('koa')
+const path = require('path')
 const open = require('open')
 const router = require('./src/routers/index')
 const cors = require('@koa/cors')
 const koaBody = require('koa-body')
+const helmet = require('koa-helmet')
+const serve = require('koa-static')
 
 const app = new Koa()
 
+app.use(helmet())
 app.use(koaBody())
 app.use(cors())
+app.use(serve(path.join(__dirname, 'static')))
+console.log(path.join(__dirname, 'static'))
 
 // 全局中间件
 app.use(async (ctx, next) => {
   ctx.token = 'loewe0202'
   await next()
 })
+
 app.use(router())
 
 app.listen(3000, () => {
